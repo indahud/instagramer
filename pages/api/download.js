d0491de58291e1578a22f7042bf6ee55b2cfb9ec
indahud <indahud@protonmail.com>
2020-12-31 05:45:05 +0100

    api

--- pages/api/download.js
+++ pages/api/download.js
@@ -8,24 +8,26 @@ export default async (req, res) => {
       newUrl.substr(newUrl.length - 1) === "/" ? newUrl.slice(0, -1).trim() : ""
 
     const fetchData = await axios(`${newUrl}/?__a=1`)
-    const rawData = await fetchData.data
     res.setHeader("Content-Type", "application/json")
-    console.log(rawData)
-    if (rawData.graphql.shortcode_media.is_video) {
-      const videoResult = await rawData.graphql.shortcode_media.video_url
-      const videoThumbnail = await rawData.graphql.shortcode_media.display_url
+    const rawData = await fetchData.data
+    res.json({
+      response: rawData
+    })
+    // if (rawData.graphql.shortcode_media.is_video) {
+    //   const videoResult = await rawData.graphql.shortcode_media.video_url
+    //   const videoThumbnail = await rawData.graphql.shortcode_media.display_url
 
-      return res.status(200).json({
-        resData: videoResult,
-        thumbnailData: videoThumbnail
-      })
-    } else {
-      const Imageresult = await rawData.graphql.shortcode_media.display_url
-      const imageThumbnail = await rawData.graphql.shortcode_media.display_url
-      return res.status(200).json({
-        resData: Imageresult,
-        thumbnailData: imageThumbnail
-      })
-    }
+    //   return res.status(200).json({
+    //     resData: videoResult,
+    //     thumbnailData: videoThumbnail
+    //   })
+    // } else {
+    //   const Imageresult = await rawData.graphql.shortcode_media.display_url
+    //   const imageThumbnail = await rawData.graphql.shortcode_media.display_url
+    //   return res.status(200).json({
+    //     resData: Imageresult,
+    //     thumbnailData: imageThumbnail
+    //   })
+    // }
 
 }
